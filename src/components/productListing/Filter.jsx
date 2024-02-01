8b1c99a6589ad9884d9a2ca23f7a161742bda3fe
Aajugiya Ravi <aajugiyaravi@gmail.com>
2024-02-01 07:38:09 +0530

    filtering products completed

--- src/components/productListing/Filter.jsx
+++ src/components/productListing/Filter.jsx
@@ -13,43 +13,36 @@ function Filter({ title, items }) {
   useEffect(() => {
     const categories = searchParams.getAll("category");
     setSelectedCategories(categories);
-
+    
     const minPrice = parseFloat(searchParams.get("minprice")) || 10;
     const maxPrice = parseFloat(searchParams.get("maxprice")) || 20000;
     setPriceRange([minPrice, maxPrice]);
-  }, [searchParams]);
+  }, []);
 
   const handleCheckboxChange = (itemId) => {
     const updatedCategories = selectedCategories.includes(itemId)
       ? selectedCategories.filter((id) => id !== itemId)
       : [...selectedCategories, itemId];
 
-    setSearchParams({ category: updatedCategories });
+    const categoryParam = updatedCategories.join(",");
+    setSearchParams({ category: categoryParam, minprice: priceRange[0], maxprice: priceRange[1] }, { replace: true });
 
     setSelectedCategories(updatedCategories);
   };
 
   const handlePriceRangeChange = (value) => {
-    setSearchParams({
-      minprice: value[0].toString(),
-      maxprice: value[1].toString(),
-      category: selectedCategories,
-    });
-
+    setSearchParams({ category: selectedCategories.join(","), minprice: value[0], maxprice: value[1] }, { replace: true });
     setPriceRange(value);
   };
 
   return (
     <>
       <div className="flex w-full flex-col p-3 text-primary-color">
-        <div
-          onClick={() => setIsItemVisible(!isItemVisible)}
-          className="flex justify-between items-center cursor-pointer"
-        >
+        <div onClick={() => setIsItemVisible(!isItemVisible)} className="flex justify-between items-center cursor-pointer">
           <h3 className="text-lg font-semibold">{title}</h3>
           {isItemVisible ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
         </div>
-        {isItemVisible ? (
+        {isItemVisible && (
           <div className="flex flex-col mt-2">
             {items?.map((item) => (
               <div className="flex gap-2" key={item._id}>
@@ -64,7 +57,7 @@ function Filter({ title, items }) {
               </div>
             ))}
           </div>
-        ) : null}
+        )}
       </div>
       <div className="p-3 text-primary-color">
         <h3 className="text-lg font-semibold mb-2">Filter by Price</h3>
@@ -75,7 +68,7 @@ function Filter({ title, items }) {
           name="priceRange"
           min={10}
           max={20000}
-          step={10}
+          step={1000}
           defaultValue={priceRange}
           value={priceRange}
           onInput={handlePriceRangeChange}
--- src/components/productListing/ProductListing.jsx
+++ src/components/productListing/ProductListing.jsx
@@ -3,18 +3,52 @@ import Product from "../home/Product";
 import Filter from "./Filter";
 import { useGetListingProductQuery } from "../../redux/api/productApi";
 import { useGetAllCategoriesQuery } from "../../redux/api/categoryApi";
-
+import { useSearchParams } from "react-router-dom";
 
 function ProductListing() {
   const [products, setProducts] = useState([]);
-  const [priceRange, setPriceRange] = useState([10, 20000]);
   const { data: categoryData } = useGetAllCategoriesQuery();
-
   const { data: productsData } = useGetListingProductQuery({
     page: 1,
     limit: 100,
   });
 
+  useEffect(() => {
+    setProducts(productsData?.data?.products)
+    console.log(products);
+  }, [productsData])
+  
+  const [searchParams] = useSearchParams();
+  useEffect(() => {
+    console.log("Query parameters:", Object.fromEntries(searchParams));
+    const params = Object.fromEntries(searchParams);
+    const categories = params.category ? params.category.split(",") : []; 
+    console.log(categories);
+  
+    const fetchProductsPromises = categories.map((categoryId) => {
+      return fetchProductsByCategory(categoryId);
+    });
+  
+    Promise.all(fetchProductsPromises)
+      .then((productsByCategory) => {
+        const combinedProducts = productsByCategory.flat();
+        console.log(params.minPrice);
+        const filteredProducts = combinedProducts.filter(product => {
+          return product.price >= params.minprice && product.price <= params.maxprice;
+        });
+        // console.log("Combined Products:", filteredProducts);
+        setProducts(filteredProducts);
+      })
+  }, [searchParams]);
+  
+  const fetchProductsByCategory = async (categoryId) => {
+    const response = await fetch(
+      `http://localhost:8080/api/v1/ecommerce/products/category/${categoryId}`
+    );
+  
+    const data = await response.json();
+    return data.data.products;
+  };
 
   return (
     <div>
@@ -24,17 +58,9 @@ function ProductListing() {
             title="Filter By Category"
             items={categoryData?.data?.categories}
           />
-          
         </div>
         <div>
-          {/* <div className="flex justify-between px-9"> */}
-          {/* <div className="flex gap-5 mb-3 justify-end"> */}
-          {/* <IoGridOutline className="size-6" /> */}
-          {/* <TfiLayoutListThumb className="size-6" /> */}
-          {/* <p className="text-right">72 results</p> */}
-          {/* </div> */}
-          {/* </div> */}
-          <div className="px-5 grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-5 mx-auto ">
+          <div className="px-5 grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-5 mx-auto">
             {products?.map((product) => (
               <Product
                 key={product.id}
